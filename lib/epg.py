74bfba53b85a181b0f9b5e28eac67fbc58aefc3d
nano-f <2996460+nano-f@users.noreply.github.com>
2018-09-06 11:52:56 +0100

    Add quality switch in context menu

--- lib/epg.py
+++ lib/epg.py
@@ -278,6 +278,7 @@ class ViewManager(object):
             d.addItem('stop_download','Stop Recording')
         if not DownloadRegistry().empty():
             d.addItem('view_recordings','View Recordings')
+        d.addItem('changeQuality', 'Change Quality')
         d.addItem('changeView','Change View')
         d.addItem('settings','Settings')
         if (util.getSetting('back_opens_context',False) or util.getSetting('show_fullscreen_option',False)) and xbmc.getCondVisibility('Player.HasVideo'):
@@ -315,10 +316,21 @@ class ViewManager(object):
             self.changeTheme(selection)
         elif selection == 'changeView':
             self.changeView(selection)
+        elif selection == 'changeQuality':
+            self.changeQuality(selection)
         elif selection == 'exit':
             self.mode = None
             self.window.close()
             if util.getSetting('fullscreen_on_exit',True): self.fullscreenVideo()
+    
+    def changeQuality(self,quality):
+        dialog = util.xbmcDialogSelect('Change Quality')
+        dialog.addItem('hd','High Definition')
+        dialog.addItem('ld','Low Definition')
+        result = dialog.getResult()
+
+        util.setSetting('high_def', result == 'hd')
+        return
 
     def changeView(self,view):
         dialog = util.xbmcDialogSelect('Change View')
